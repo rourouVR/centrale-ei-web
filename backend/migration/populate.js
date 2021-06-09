e2751cd5aa3046b3c09c77ea4f05ee0f20eb516a
2020kacemr <rouaida.kacem@student-cs.fr>
2021-06-09 09:31:08 +0200

    populate the database

--- backend/migration/populate.js
+++ backend/migration/populate.js
@@ -6,22 +6,27 @@ mongoose.connect(process.env.MONGO_DB_URL, {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
+for (let pas = 41; pas < 80; pas++) {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=522d421671cf75c2cba341597d86403a&language=en-US&page=" +
+        pas
+    )
+    .then((response) => {
+      for (const movie of response.data.results) {
+        console.log(movie);
+        const newmovie = new movieModel({
+          imdbid: movie.id,
+          title: movie.title,
+          release_date: movie.release_date,
+          description: movie.overview,
+          genre: movie.genre_ids,
+          note: movie.vote_average,
+          imageurl: movie.poster_path,
+          language: movie.original_language,
+        });
 
-axios
-  .get(
-    "https://api.themoviedb.org/3/movie/top_rated?api_key=522d421671cf75c2cba341597d86403a&language=en-US&page=2"
-  )
-  .then((response) => {
-    for (const movie of response.data.results) {
-      console.log(movie);
-      const newmovie = new movieModel({
-        title: movie.title,
-        release_date: movie.release_date,
-        description: movie.overview,
-        genre: movie.genre_ids,
-        imageurl: movie.poster_path,
-      });
-
-      newmovie.save();
-    }
-  });
+        newmovie.save();
+      }
+    });
+}
--- backend/models/movies.js
+++ backend/models/movies.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
+  imdbid: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   release_date: { type: String },
   description: { type: String },
   genre: { type: Array },
+  note: { type: String },
   imageurl: { type: String },
+  language: { type: String },
 });
 
 const movieModel = mongoose.model("movieModel", movieSchema, "movies");
